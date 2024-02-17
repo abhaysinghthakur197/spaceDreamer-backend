34432d83aa71745df994d22e56c906b73a7a337d
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-02-17 12:51:43 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connectToDb(url) {
-    return mongoose.connect(url, {
-        useUnifiedTopology:true,
-        useNewUrlParser:true})
+    return mongoose.connect(url)
 }
 
 module.exports = connectToDb;
